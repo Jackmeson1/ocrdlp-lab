e5954a3020ef59d009fb5c5e7279988781450ac8
Jackmeson1 <130693642+Jackmeson1@users.noreply.github.com>
2025-06-04 20:46:26 -0700

    chore: clean crawler package

--- crawler/__init__.py
+++ crawler/__init__.py
@@ -6,4 +6,4 @@ from .image_crawler import ImageCrawler
 from .filters import ImageFilter
 from .deduplicator import ImageDeduplicator
 
-__all__ = ["ImageCrawler", "ImageFilter", "ImageDeduplicator"] 
+__all__ = ["ImageCrawler", "ImageFilter", "ImageDeduplicator"]
